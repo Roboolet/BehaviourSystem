e7a36c0d8a3f8ce809423bb0a27bba7783ec7924
Lea <39883637+Roboolet@users.noreply.github.com>
2024-12-23 13:36:38 +0100

    Initial commit

---
+++ Assets/Behaviour System/Nodes/Blackboard.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Blackboard
+{
+    public void Set(string key, object value)
+    {
+        
+    }
+}
---
+++ Assets/Behaviour System/Nodes/INode.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public interface INode
+{
+    public NodeReturnState Execute(Blackboard bb);
+}
+
+public abstract class ANode : INode
+{
+    public NodeReturnState Execute(Blackboard bb)
+    {
+        if (bb == null)
+        {
+            Debug.LogError("This node does not have a Blackboard reference");
+            return NodeReturnState.ERROR;
+        }
+        else
+        {
+            bb.Set("current_node", this);
+        }
+        
+        return OnExecute(bb);
+    }
+
+    protected NodeReturnState OnExecute(Blackboard bb)
+    {
+        Debug.LogWarning("This node has no implemented behaviour");
+        return NodeReturnState.ERROR;
+    }
+}
+
+public enum NodeReturnState
+{
+    ERROR,
+    FAILED,
+    SUCCESS,
+    RUNNING
+}
