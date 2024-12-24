bdb860a2da869be1d3219cdeac6c19849995829f
Lea <39883637+Roboolet@users.noreply.github.com>
2024-12-24 15:13:22 +0100

    Blackboard code

--- Assets/Behaviour System/Nodes/Blackboard.cs
+++ Assets/Behaviour System/Nodes/Blackboard.cs
@@ -1,9 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class Blackboard
 {
+    public static Blackboard global;
+    private Hashtable objects;
+
+    public Blackboard()
+    {
+        objects = new Hashtable();
+    }
+    
     public void Set(string key, object value)
     {
-        
+        if (objects.ContainsKey(key))
+        {
+            objects[key] = value;
+        }
+        else
+        {
+            objects.Add(key, value);
+        }
     }
+
+    public bool TryGet<T>(string key, out T value)
+    {
+        if (!objects.ContainsKey(key))
+        {
+            value = default(T);
+            return false;
+        }
+        else
+        {
+            value = (T)objects[key];
+            return true;
+        }
+    }
+
+    public bool ContainsKey(string key) => objects.ContainsKey(key);
 }
