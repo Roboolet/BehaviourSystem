0d02734aaee1919f91039b55fd8ac2f853666de0
Lea <39883637+Roboolet@users.noreply.github.com>
2025-01-14 10:51:07 +0100

    Added SequenceNode

---
+++ Assets/Behaviour System/Common Nodes/SequenceNode.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SequenceNode : CompositeNode
+{
+    protected override NodeReturnState OnExecute(Blackboard bb)
+    {
+        NodeReturnState ret = children[index].Execute(bb);
+
+        switch (ret)
+        {
+            default: return ret;
+            
+            case NodeReturnState.SUCCESS:
+                index++;
+                if (index >= children.Length)
+                {
+                    return NodeReturnState.SUCCESS;
+                }
+                // if not yet past every node, return running
+                return NodeReturnState.RUNNING;
+            
+            // if errored or failed, reset index
+            case NodeReturnState.ERROR:
+            case NodeReturnState.FAILED:
+                index = 0;
+                return ret;
+                
+        }
+    }
+}
