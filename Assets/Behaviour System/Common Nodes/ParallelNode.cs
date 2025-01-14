ae903e60111f84132284be17aee48e45ae7491f8
Lea <39883637+Roboolet@users.noreply.github.com>
2025-01-14 10:58:38 +0100

    Added SelectorNode

---
+++ Assets/Behaviour System/Common Nodes/ParallelNode.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ParallelNode : CompositeNode
+{
+    protected override NodeReturnState OnExecute(Blackboard bb)
+    {
+        return base.OnExecute(bb);
+    }
+}
---
+++ Assets/Behaviour System/Common Nodes/SelectorNode.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SelectorNode : CompositeNode
+{
+    protected override NodeReturnState OnExecute(Blackboard bb)
+    {
+        NodeReturnState ret = children[index].Execute(bb);
+
+        switch (ret)
+        {
+            default: return ret;
+            
+            case NodeReturnState.SUCCESS:
+                index = 0;
+                return NodeReturnState.SUCCESS;
+            
+            case NodeReturnState.ERROR:
+                index = 0;
+                return NodeReturnState.ERROR;
+            
+            case NodeReturnState.FAILED:
+                index++;
+                index %= children.Length;
+                return ret;
+                
+        }
+    }
+}
