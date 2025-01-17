b5c958df9d71789662b4acc57ac7ff352002e70d
Lea <39883637+Roboolet@users.noreply.github.com>
2025-01-17 15:07:26 +0100

    Quick and dirty player controller

--- Assets/Content/Player/Player.cs
+++ Assets/Content/Player/Player.cs
@@ -1,16 +1,46 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
-    [SerializeField] private float moveSpeed;
-    
+    [SerializeField] private float moveSpeed, gravityScale, jumpForce, mouseSens;
+
+    private float xVelocity, yVelocity, zVelocity;
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveVec = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        moveVec = moveVec * Time.deltaTime * moveSpeed;
+        // move the player
+        xVelocity = Input.GetAxis("Horizontal") * (Time.deltaTime * moveSpeed);
+        zVelocity = Input.GetAxis("Vertical") * (Time.deltaTime * moveSpeed);
         
-        controller.Move(moveVec);
+        if (controller.isGrounded)
+        {
+            yVelocity = Mathf.Max(0, yVelocity);
+            if (Input.GetButton("Jump"))
+            {
+                yVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            yVelocity -= Time.deltaTime * gravityScale;
+        }
+        controller.Move(transform.right * xVelocity +
+                        Vector3.up * yVelocity +
+                        transform.forward * zVelocity);
+        
+        // move the camera
+        transform.Rotate(0,Input.GetAxis("Mouse X") * mouseSens, 0);
+        cam.transform.Rotate(-Input.GetAxis("Mouse Y") * mouseSens, 0, 0);
     }
 }
