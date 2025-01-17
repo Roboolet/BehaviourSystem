f662316bf4d9eff35f359a9d4f906e634ed6e615
Lea <39883637+Roboolet@users.noreply.github.com>
2025-01-17 15:52:45 +0100

    Added PatrolRoute Monobehaviour

---
+++ Assets/Behaviour System/PatrolRoute.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class PatrolRoute : MonoBehaviour
+{
+    [SerializeField] private Agent[] agents;
+    [SerializeField] private string blackboardKey = "patrol_route";
+
+    private Transform[] points;
+
+    private void Start()
+    {
+        // this behaviour does nothing by itself, it has to be
+        // interacted with by the agents
+        for (int i = 0; i < agents.Length; i++)
+        {
+            agents[i].blackboard.Set(blackboardKey, this);
+        }
+
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+
+    public Transform GetClosestWaypoint(Vector3 position)
+    {
+        Transform closest = null;
+        float closestDist = Mathf.Infinity;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform tf = points[i];
+            float dist = (tf.position - position).sqrMagnitude;
+            if (dist < closestDist)
+            {
+                closestDist = dist;
+                closest = tf;
+            }
+        }
+
+        return closest;
+    }
+
+    public int GetIndexOfWaypoint(Transform waypoint)
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            if (points[i] == waypoint)
+            {
+                return i;
+            }
+        }
+
+        Debug.LogError("Waypoint " + waypoint.name + " not found in " + transform.name);
+        return -1;
+    }
+
+    public Transform GetNextWaypoint(int currentIndex)
+    {
+        return points[(currentIndex + 1) % points.Length];
+    }
+}
--- Assets/Content/Slime/SlimeBlueprint.cs
+++ Assets/Content/Slime/SlimeBlueprint.cs
@@ -7,6 +7,7 @@ public class SlimeBlueprint : BehaviourBlueprint
     private const string TARGET = "slime_target";
     private const string TARGET_DISTANCE = "slime_target_distance";
     private const string SIZE = "slime_size";
+    private const string IS_PATROLLING = "slime_is_patrolling";
 
     [SerializeField] private LayerMask targetLineOfSightLayer;
     [SerializeField] private float sizeMinimum, sizeThreshold, attackDistance;
@@ -19,6 +20,7 @@ public class SlimeBlueprint : BehaviourBlueprint
             {
                 new NMoveTowards(TARGET, PositionReadMode.GAME_OBJECT),
                 new NGetDistanceTo(TARGET, TARGET_DISTANCE, PositionReadMode.GAME_OBJECT),
+                // if close to player, attack
                 new NDComparison<float>(new NAttackPlayer(), TARGET_DISTANCE, Comparator.GREATER,
                     attackDistance, true)
             }), SIZE, Comparator.GREATER, sizeThreshold);
@@ -30,7 +32,14 @@ public class SlimeBlueprint : BehaviourBlueprint
                 new NCSelector(
                     new INode[]
                     {
-                        
+                        // if not patrolling, go to closest patrol point
+                        new NDReadBool(
+                            new NCSequence(
+                                new INode[]
+                                {   
+                                    new NBlackboardSet(IS_PATROLLING, true)
+                                })
+                            , IS_PATROLLING, true)
                     })
             });
         
@@ -40,6 +49,7 @@ public class SlimeBlueprint : BehaviourBlueprint
             new NCSelector(
                 new INode[]
             {
+                // the slime sees the player, now decide to run or attack
                 new NDHasLineOfSight(
                     new NCSequence(
                         new INode[]
