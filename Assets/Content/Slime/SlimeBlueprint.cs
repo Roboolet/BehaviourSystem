afe3045563aa3dc692aca418a4e674238e40a517
Lea <39883637+Roboolet@users.noreply.github.com>
2025-05-26 00:34:19 +0200

    Updated SlimeBlueprint

--- Assets/Content/Slime/SlimeBlueprint.cs
+++ Assets/Content/Slime/SlimeBlueprint.cs
@@ -16,8 +16,30 @@ public class SlimeBlueprint : BehaviourBlueprint
     
     public override INode BuildTree()
     {
+        // the blue section on the diagram
+        INode branch_patrolling = new NCSequence(
+            new NCSelector(
+                // if not patrolling, go to closest patrol point
+                new NDReadBool(
+                    new NCSequence(
+                        new NGetClosestPatrolPoint(TARGET),
+                        new NBlackboardSet(IS_PATROLLING, true))
+                    , IS_PATROLLING, true), 
+                new NCSequence(
+                    // when close to target patrol point, go to the next patrol point
+                    new NGetClosestPatrolPoint(TARGET),
+                    new NGetDistanceTo(TARGET, TARGET_DISTANCE, PositionReadMode.GAME_OBJECT),
+                    new NDComparison<float>(
+                        new NGetNextPatrolPoint(TARGET, TARGET),
+                        TARGET_DISTANCE, Comparator.GREATER, 2, true))
+            ),
+            new NPrint("Moving towards patrol point"),
+            new NMoveTowards(TARGET, PositionReadMode.GAME_OBJECT));
+        
         // the red section on the diagram
-        INode branch_chasing = new NDComparison<float>(new NCSelector(
+        INode branch_chasing = new NDComparison<float>(
+            new NCSelector(
+                new NPrint("Moving towards player"),
                 new NMoveTowards(TARGET, PositionReadMode.GAME_OBJECT),
                 new NGetDistanceTo(TARGET, TARGET_DISTANCE, PositionReadMode.GAME_OBJECT),
                 // if close to player, attack
@@ -25,38 +47,20 @@ public class SlimeBlueprint : BehaviourBlueprint
                     attackDistance, true)
             ), SIZE, Comparator.GREATER, sizeThreshold);
 
-        // the blue section on the diagram
-        INode branch_patrolling = new NCSequence(
-                new NCSelector(
-                        // if not patrolling, go to closest patrol point
-                        new NDReadBool(
-                            new NCSequence(
-                                    new NGetClosestPatrolPoint(TARGET),
-                                    new NBlackboardSet(IS_PATROLLING, true))
-                            , IS_PATROLLING, true), 
-                        new NCSequence(
-                            // when close to target patrol point, go to the next patrol point
-                            new NGetClosestPatrolPoint(TARGET),
-                            new NGetDistanceTo(TARGET, TARGET_DISTANCE, PositionReadMode.GAME_OBJECT),
-                            new NDComparison<float>(
-                                new NGetNextPatrolPoint(TARGET, TARGET),
-                                TARGET_DISTANCE, Comparator.GREATER, 2, true))
-                ),
-                new NMoveTowards(TARGET, PositionReadMode.GAME_OBJECT));
-
         // the yellow section on the diagram
-        INode branch_fleeing = new NCSelector(
+        INode branch_fleeing = new NDComparison<float>(new NCSelector(
             // fusing
             new NCSequence(
                 new NGetAgents(ALL_SLIMES, "Enemy"),
                 new NGetClosestGameObjectInList(ALL_SLIMES, TARGET),
                 new NCSequence(
+                    new NPrint("Moving towards other slime"),
                     new NMoveTowards(TARGET, PositionReadMode.GAME_OBJECT),
                     new NGetDistanceTo(TARGET, TARGET_DISTANCE, PositionReadMode.GAME_OBJECT),
                     new NDComparison<float>(
                         new NSlimeFuse(TARGET, SIZE), 
                         TARGET_DISTANCE, Comparator.GREATER, fuseDistance, true)))
-        );
+        ), SIZE, Comparator.GREATER, sizeThreshold, true);
 
         INode root = new NCSequence(
             new NGetGameObjectWithTag("Player", TARGET),
@@ -70,7 +74,7 @@ public class SlimeBlueprint : BehaviourBlueprint
                         branch_chasing,
                         branch_fleeing)), HAS_LOS)
                 ,
-                branch_patrolling));
+                new NDReadBool(branch_patrolling, HAS_LOS, true)));
         
         return root;
     }
